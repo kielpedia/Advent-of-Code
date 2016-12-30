f871e52f4ea127904fa2ae54c922b11c34327a61
Kiel Loysen <kloysen@grubhub.com>
2016-12-30 12:22:32 -0600

    add part 2 for day 3

--- 2016/day3/lib/day3.ex
+++ 2016/day3/lib/day3.ex
@@ -2,7 +2,6 @@ defmodule Day3 do
 
     def get_valid_triangle_count(triangles) do
         triangles
-        |> Enum.map(&String.split(&1))
         |> Enum.map(&is_valid?(&1))
         |> Enum.filter(&(&1))
         |> Enum.count
@@ -18,14 +17,30 @@ defmodule Day3 do
         path
         |> File.read!
         |> String.split("\n")
+        |> Enum.map(&String.split(&1))
         |> get_valid_triangle_count
     end
 
-#    @doc "get the code from the file for part 2 of challenge"
-#    def solve_with_file_part_2(path) do
-#        path
-#        |> File.read!
-#        |> String.split("\n")
-##        |> get_code_from_key(@keypad2)
-#    end
+    @doc "get the code from the file for part 2 of challenge"
+    def solve_with_file_part_2(path) do
+        path
+        |> File.read!
+        |> String.split("\n")
+        |> group_triangles_by_column
+        |> get_valid_triangle_count
+    end
+
+    defp group_triangles_by_column(lines, triangles \\ [])
+    defp group_triangles_by_column([one | [two | [three | tail]]], triangles) do
+        triangles = triangles ++ zip([String.split(one), String.split(two), String.split(three)])
+        group_triangles_by_column(tail, triangles)
+    end
+    defp group_triangles_by_column(_, triangles) do triangles end
+
+    # zip all enumerables - needed until Elixir 1.4
+    defp zip([head | []]) do Enum.map(head, &List.flatten(&1)) end
+    defp zip([head | [head2 | tail]]) do
+      zip([head |> Enum.zip(head2) |> Enum.map(&Tuple.to_list(&1)) | tail])
+    end
+
 end
